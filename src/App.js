6f3de2f630ee0e23e6555b60837e8e6b79c1e9f7
isaacpitwa <isaacpitwa256@gmail.com>
2022-04-20 00:58:30 +0300

    make App.js class based component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
+import React from 'react';
 import Calculator from './components/calculator';
 
-function App() {
-  return (
-    <Calculator />
-  );
+class App extends React.PureComponent {
+  render() {
+    return (
+      <Calculator />
+    );
+  }
 }
 
 export default App;
