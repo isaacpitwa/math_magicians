a9497d98fd6a5975d15a5ff87711c6a64a0d3182
isaacpitwa <isaacpitwa256@gmail.com>
2022-04-27 01:57:54 +0300

    set up react router

--- README.md
+++ README.md
@@ -1,9 +1,7 @@
 ![](https://img.shields.io/badge/Microverse-blueviolet)
 # Math magicians
 
->"Math magicians" is a website for all fans of mathematics. It is a Single Page App (SPA) that allows users to:
- - Make simple calculations.
- - Read a random math-related quote.
+> TODO List you will build a React app list of To Do tasks. 
 
 <!-- ## Live preview
 
--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import './App.css';
 import React from 'react';
+import { Route, Routes } from 'react-router-dom';
 import Calculator from './components/calculator';
+import Home from './pages/home';
 
 class App extends React.PureComponent {
   render() {
     return (
-      <Calculator />
+      <Routes>
+        <Route path="/" component={<Home />} />
+        <Route path="calculator" component={<Calculator />} />
+      </Routes>
     );
   }
 }
