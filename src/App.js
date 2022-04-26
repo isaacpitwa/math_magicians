1f60628bb345ec443e39f3a9cf6335101394184d
isaacpitwa <isaacpitwa256@gmail.com>
2022-04-27 02:19:28 +0300

    Add quotes app

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { Route, Routes } from 'react-router-dom';
 import Calculator from './components/calculator';
 import Home from './pages/home';
 import Header from './components/header';
+import Quote from './pages/quote';
 
 class App extends React.PureComponent {
   render() {
@@ -13,6 +14,7 @@ class App extends React.PureComponent {
         <Routes>
           <Route path="/" element={<Home />} exact />
           <Route path="calculator" element={<Calculator />} />
+          <Route path="quote" element={<Quote />} />
         </Routes>
       </div>
     );
---
+++ src/pages/quote.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+function Quote() {
+  return (
+    <div>
+      <h3>Quotes are here</h3>
+    </div>
+  );
+}
+export default Quote;
