71f9d4a17cbe3ec1b4315ef4e02b7ba90e65d2d4
isaacpitwa <isaacpitwa256@gmail.com>
2022-04-27 02:52:15 +0300

    Fix anchor links styling

--- src/components/header.js
+++ src/components/header.js
@@ -5,7 +5,7 @@ import style from './header.module.css';
 function Header() {
   return (
     <header>
-      <h3>Math Magicians</h3>
+      <h3><NavLink to="/" exact="true">Math Magicians</NavLink></h3>
       <nav className={style.navlist}>
         <li className={style.navlink}><NavLink to="/" exact="true">Home</NavLink></li>
         <li className={style.navlink}><NavLink to="/calculator" exact="true">Calculator</NavLink></li>
