7c7d1775bd87edc362232eedd89889fd05a0ca62
isaacpitwa <isaacpitwa256@gmail.com>
2022-04-27 02:17:42 +0300

    Add home page contents

--- src/pages/home.js
+++ src/pages/home.js
@@ -3,7 +3,29 @@ import React from 'react';
 function Home() {
   return (
     <div>
-      <h3>Home Page</h3>
+      <h4>Welcome  to our Page</h4>
+      <p>
+        {' '}
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+        Veritatis voluptas enim asperiores distinctio aspernatur molestiae unde fuga.
+        Fuga, sed labore odit voluptatem porro sunt saepe aliquid quaerat doloremque incidunt autem.
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+        Veritatis voluptas enim asperiores distinctio aspernatur molestiae unde fuga.
+        Fuga, sed labore odit voluptatem porro sunt saepe aliquid quaerat doloremque incidunt autem.
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+        Veritatis voluptas enim asperiores distinctio aspernatur molestiae unde fuga.
+      </p>
+      <p>
+        {' '}
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+        Veritatis voluptas enim asperiores distinctio aspernatur molestiae unde fuga.
+        Fuga, sed labore odit voluptatem porro sunt saepe aliquid quaerat doloremque incidunt autem.
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+        Veritatis voluptas enim asperiores distinctio aspernatur molestiae unde fuga.
+        Fuga, sed labore odit voluptatem porro sunt saepe aliquid quaerat doloremque incidunt autem.
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+        Veritatis voluptas enim asperiores distinctio aspernatur molestiae unde fuga.
+      </p>
     </div>
   );
 }
